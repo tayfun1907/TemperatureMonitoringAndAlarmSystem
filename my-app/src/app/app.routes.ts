742d55fbf9816770c4a84d99972ffd649a00d28c
Tayfun Can ER <77841264+tayfun1907@users.noreply.github.com>
2026-02-01 01:30:37 +0300

    Delete my-app directory

--- my-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TemperatureDashboardComponent } from './components/temperature-dashboard/temperature-dashboard';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: TemperatureDashboardComponent
-  }
-];
